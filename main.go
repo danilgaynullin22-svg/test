f441e128e4950093dbd98260466f8f00a0411c5d
Danil <danilgaynullin22@gmail.com>
2026-08-23 19:54:56 +0500

    test2.1

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
 
+func worker(wg *sync.WaitGroup, i int) {
+	defer wg.Done()
+
+	randomTime := (rand.Intn(5)+1) * 100
+	fmt.Println("Worker", i)
+
+	time.Sleep( time.Duration(randomTime) * time.Millisecond)
+
+	fmt.Println("Готово")
+}
 func main() {
-	fmt.Println("hello")
-	fmt.Println("hello world")
-	fmt.Println("hello world")
-	fmt.Println("hello world")
+	wg := &sync.WaitGroup{}
+	for i := range 5{
+		wg.Add(1)
+		go worker(wg, i+1)
+	}
+
+	wg.Wait()
 }
